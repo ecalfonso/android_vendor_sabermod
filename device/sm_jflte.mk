4502e51dcf7668ecd165638869f784141d8eff31
Eduard <xsynergy510x@gmail.com>
2015-07-01 12:51:14 -0700

    jflte: Build ROM with GCC4.8

--- device/sm_jflte.mk
+++ device/sm_jflte.mk
@@ -24,7 +24,7 @@ endif
 ifeq ($(strip $(HOST_OS)),linux)
 
   # Sabermod configs
-  TARGET_SM_AND := 4.9
+  TARGET_SM_AND := 4.8
   TARGET_SM_KERNEL := 4.9
   JFLTE_THREADS := 4
   PRODUCT_THREADS := $(JFLTE_THREADS)
