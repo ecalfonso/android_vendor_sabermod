941b69a40ae5715764ced5f58a0aaa1508a33a84
Eduard Alfonso <xsynergy510x@gmail.com>
2015-07-31 10:12:00 -0700

    Revert "configs/sm.mk: Add -fno-inline-functions to resolve possible cache issues"
    This reverts commit 9ab8fe7953a77341781da5bf3c77424cf507bee1.

--- configs/sm.mk
+++ configs/sm.mk
@@ -578,7 +578,6 @@ ifeq ($(strip $(LOCAL_O3)),true)
   # -O3 flags and friends
   O3_FLAGS := \
     -O3 \
-    -fno-inline-functions \
     -Wno-error=array-bounds \
     -Wno-error=strict-overflow
 else
